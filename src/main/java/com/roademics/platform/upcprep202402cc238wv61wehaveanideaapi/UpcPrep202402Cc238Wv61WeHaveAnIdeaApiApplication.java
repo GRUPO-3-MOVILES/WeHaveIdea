2085a4451325f1d0668047d8cb6b2ea986d11b59
JuanPescoran <fabritzziopescoran@gmail.com>
2024-09-14 16:35:34 -0500

    Initial commit

---
+++ src/main/java/com/roademics/platform/upcprep202402cc238wv61wehaveanideaapi/UpcPrep202402Cc238Wv61WeHaveAnIdeaApiApplication.java
@@ -0,0 +1,13 @@
+package com.roademics.platform.upcprep202402cc238wv61wehaveanideaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpcPrep202402Cc238Wv61WeHaveAnIdeaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UpcPrep202402Cc238Wv61WeHaveAnIdeaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/roademics/platform/upcprep202402cc238wv61wehaveanideaapi/UpcPrep202402Cc238Wv61WeHaveAnIdeaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roademics.platform.upcprep202402cc238wv61wehaveanideaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UpcPrep202402Cc238Wv61WeHaveAnIdeaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
