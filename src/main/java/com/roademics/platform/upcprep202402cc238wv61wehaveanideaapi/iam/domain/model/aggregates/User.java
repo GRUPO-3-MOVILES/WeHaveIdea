563a45cf85acf62a3b6188c9825aa44e623c048c
JuanPescoran <fabritzziopescoran@gmail.com>
2024-09-15 03:41:28 -0500

    feat(iam): add aggregate, entity and value object

---
+++ src/main/java/com/roademics/platform/upcprep202402cc238wv61wehaveanideaapi/iam/domain/model/aggregates/User.java
@@ -0,0 +1,4 @@
+package com.roademics.platform.upcprep202402cc238wv61wehaveanideaapi.iam.domain.model.aggregates;
+
+public class User {
+}
--- src/main/java/com/roademics/platform/upcprep202402cc238wv61wehaveanideaapi/iam/domain/model/commands/SignUpCommand.java
+++ src/main/java/com/roademics/platform/upcprep202402cc238wv61wehaveanideaapi/iam/domain/model/commands/SignUpCommand.java
@@ -1,5 +1,7 @@
 package com.roademics.platform.upcprep202402cc238wv61wehaveanideaapi.iam.domain.model.commands;
 
+import com.roademics.platform.upcprep202402cc238wv61wehaveanideaapi.iam.domain.model.entities.Role;
+
 import java.util.List;
 
 public record SignUpCommand(String username, String password, List<Role> roles) {
