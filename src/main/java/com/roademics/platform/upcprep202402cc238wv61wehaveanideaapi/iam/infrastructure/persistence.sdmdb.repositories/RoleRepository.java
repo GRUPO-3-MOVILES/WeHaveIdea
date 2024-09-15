b772481b7f9265fa7cdd7755547df926a8d0c3c9
JuanPescoran <fabritzziopescoran@gmail.com>
2024-09-15 03:51:39 -0500

    feat(iam): add repositories into infrastructure using spring data mongodb

---
+++ src/main/java/com/roademics/platform/upcprep202402cc238wv61wehaveanideaapi/iam/infrastructure/persistence.sdmdb.repositories/RoleRepository.java
@@ -0,0 +1,15 @@
+package com.roademics.platform.upcprep202402cc238wv61wehaveanideaapi.iam.infrastructure.persistence.sdmdb.repositories;
+
+import com.roademics.platform.upcprep202402cc238wv61wehaveanideaapi.iam.domain.model.entities.Role;
+import com.roademics.platform.upcprep202402cc238wv61wehaveanideaapi.iam.domain.model.valueobjects.Roles;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RoleRepository extends MongoRepository<Role, String> {
+    Optional<Role> findByName(Roles name);
+    boolean existsByName(Roles name);
+}
+
---
+++ src/main/java/com/roademics/platform/upcprep202402cc238wv61wehaveanideaapi/iam/infrastructure/persistence.sdmdb.repositories/UserRepository.java
@@ -0,0 +1,14 @@
+package com.roademics.platform.upcprep202402cc238wv61wehaveanideaapi.iam.infrastructure.persistence.sdmdb.repositories;
+
+import com.roademics.platform.upcprep202402cc238wv61wehaveanideaapi.iam.domain.model.aggregates.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+    Optional<User> findByUsername(String username);
+    boolean existsByUsername(String username);
+}
+
