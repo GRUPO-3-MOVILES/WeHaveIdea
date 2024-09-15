01ef289b8e06c608437e83fe9f784942133aab16
JuanPescoran <fabritzziopescoran@gmail.com>
2024-09-15 03:50:54 -0500

    feat(iam): add aggregate and entity into domain model

--- src/main/java/com/roademics/platform/upcprep202402cc238wv61wehaveanideaapi/iam/domain/model/aggregates/User.java
+++ src/main/java/com/roademics/platform/upcprep202402cc238wv61wehaveanideaapi/iam/domain/model/aggregates/User.java
@@ -1,4 +1,58 @@
 package com.roademics.platform.upcprep202402cc238wv61wehaveanideaapi.iam.domain.model.aggregates;
 
-public class User {
+import com.roademics.platform.upcprep202402cc238wv61wehaveanideaapi.iam.domain.model.entities.Role;
+import com.roademics.platform.upcprep202402cc238wv61wehaveanideaapi.shared.domain.model.aggregates.AuditableAbstractAggregateRoot;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@Document(collection = "users")
+public class User extends AuditableAbstractAggregateRoot<User> {
+
+    @Id
+    private String id;
+
+    @Getter
+    @NotBlank
+    @Size(max = 50)
+    private String username;
+
+    @Getter
+    @NotBlank
+    @Size(max = 120)
+    private String password;
+
+    @Getter
+    private Set<Role> roles;
+
+    public User() { this.roles = new HashSet<>(); }
+
+    public User(String username, String password) {
+        this();
+        this.username = username;
+        this.password = password;
+    }
+
+    public User(String username, String password, List<Role> roles) {
+        this(username, password);
+        addRoles(roles);
+    }
+
+    public User addRole(Role role) {
+        this.roles.add(role);
+        return this;
+    }
+
+    public User addRoles(List<Role> roles) {
+        var validatedRoles = Role.validateRoleSet(roles);
+        this.roles.addAll(validatedRoles);
+        return this;
+    }
 }
+
--- src/main/java/com/roademics/platform/upcprep202402cc238wv61wehaveanideaapi/iam/domain/model/entities/Role.java
+++ src/main/java/com/roademics/platform/upcprep202402cc238wv61wehaveanideaapi/iam/domain/model/entities/Role.java
@@ -1,4 +1,41 @@
 package com.roademics.platform.upcprep202402cc238wv61wehaveanideaapi.iam.domain.model.entities;
 
+import com.roademics.platform.upcprep202402cc238wv61wehaveanideaapi.iam.domain.model.valueobjects.Roles;
+import lombok.Getter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
+
+@Document(collection = "roles")
 public class Role {
+
+    @Id
+    private String id;
+
+    @Getter
+    private Roles name;
+
+    public Role() {}
+
+    public Role(Roles name) {
+        this.name = name;
+    }
+
+    public String getStringName() {
+        return name.name();
+    }
+
+    public static Role getDefaultRole() {
+        return new Role(Roles.ROLE_USER);
+    }
+
+    public static Role toRoleFromName(String name) {
+        return new Role(Roles.valueOf(name));
+    }
+
+    public static List<Role> validateRoleSet(List<Role> roles) {
+        if (roles == null || roles.isEmpty()) return List.of(getDefaultRole());
+        return roles;
+    }
 }
