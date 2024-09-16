e33627bc8c10b32d81bbb8b2eab5cb07344e59da
JuanPescoran <fabritzziopescoran@gmail.com>
2024-09-15 22:28:25 -0500

    feat(roadmap): add service interfaces for roadmap aggregate and both, ai interaction and ai recommendations

---
+++ src/main/java/com/roademics/platform/upcprep202402cc238wv61wehaveanideaapi/roadmaps/domain/services/RoadmapService.java
@@ -0,0 +1,21 @@
+package com.roademics.platform.upcprep202402cc238wv61wehaveanideaapi.roadmaps.domain.services;
+
+import com.roademics.platform.upcprep202402cc238wv61wehaveanideaapi.roadmaps.domain.model.aggregates.Roadmap;
+import com.roademics.platform.upcprep202402cc238wv61wehaveanideaapi.roadmaps.domain.model.commands.CreateRoadmapCommand;
+import com.roademics.platform.upcprep202402cc238wv61wehaveanideaapi.roadmaps.domain.model.commands.EndRoadmapCommand;
+import com.roademics.platform.upcprep202402cc238wv61wehaveanideaapi.roadmaps.domain.model.commands.UpdateRoadmapCommand;
+
+import java.util.List;
+
+public interface RoadmapService {
+
+    Roadmap handle(CreateRoadmapCommand command);
+
+    Roadmap handle(UpdateRoadmapCommand command);
+
+    void handle(EndRoadmapCommand command);
+
+    Roadmap getRoadmapById(String roadmapId);
+
+    List<Roadmap> getAllRoadmapsForUser(String userId);
+}
