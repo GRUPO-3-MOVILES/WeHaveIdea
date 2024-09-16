6ab57299e68457af7250c3cf33c2d08dbd7085fc
JuanPescoran <fabritzziopescoran@gmail.com>
2024-09-15 22:25:10 -0500

    feat(roadmap): add command and queries for domain model

---
+++ src/main/java/com/roademics/platform/upcprep202402cc238wv61wehaveanideaapi/roadmaps/domain/model/commands/UpdateRoadmapCommand.java
@@ -0,0 +1,4 @@
+package com.roademics.platform.upcprep202402cc238wv61wehaveanideaapi.roadmaps.domain.model.commands;
+
+public record UpdateRoadmapCommand(String roadmapId, String title, String description) {
+}
---
+++ src/main/java/com/roademics/platform/upcprep202402cc238wv61wehaveanideaapi/roadmaps/domain/model/queries/GetAllRoadmapsForProfileIdQuery.java
@@ -0,0 +1,4 @@
+package com.roademics.platform.upcprep202402cc238wv61wehaveanideaapi.roadmaps.domain.model.queries;
+
+public record GetAllRoadmapsForProfileIdQuery(String profileId) {
+}
