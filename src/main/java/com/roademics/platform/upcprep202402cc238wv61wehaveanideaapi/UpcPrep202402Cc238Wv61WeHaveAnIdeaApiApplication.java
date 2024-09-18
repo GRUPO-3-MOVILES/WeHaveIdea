8892888d726a214ccc8a4d1d5eedd75b664ae5ad
JuanPescoran <fabritzziopescoran@gmail.com>
2024-09-17 19:36:39 -0500

    feat: add mongodb string connection new driver to comunicate with
    also add mongo auditing

--- src/main/java/com/roademics/platform/upcprep202402cc238wv61wehaveanideaapi/UpcPrep202402Cc238Wv61WeHaveAnIdeaApiApplication.java
+++ src/main/java/com/roademics/platform/upcprep202402cc238wv61wehaveanideaapi/UpcPrep202402Cc238Wv61WeHaveAnIdeaApiApplication.java
@@ -2,8 +2,10 @@ package com.roademics.platform.upcprep202402cc238wv61wehaveanideaapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
 @SpringBootApplication
+@EnableMongoAuditing
 public class UpcPrep202402Cc238Wv61WeHaveAnIdeaApiApplication {
 
 	public static void main(String[] args) {
