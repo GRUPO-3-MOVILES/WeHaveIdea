8a1574b58827139e379585cc1701c3d429a75072
JuanPescoran <fabritzziopescoran@gmail.com>
2024-09-24 22:55:22 -0500

    fix: update hasing service

--- src/main/java/com/roademics/platform/upcprep202402cc238wv61wehaveanideaapi/iam/application/outboundservices/hashing/HashingService.java
+++ src/main/java/com/roademics/platform/upcprep202402cc238wv61wehaveanideaapi/iam/application/outboundservices/hashing/HashingService.java
@@ -1,5 +1,6 @@
 package com.roademics.platform.upcprep202402cc238wv61wehaveanideaapi.iam.application.outboundservices.hashing;
 
+
 public interface HashingService {
     String encode(CharSequence rawPassword);
     boolean matches(CharSequence rawPassword, String encodedPassword);
